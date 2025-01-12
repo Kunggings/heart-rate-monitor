bba7ef7e0832e903c1ffa644cbf6b0fdda16f7ed
George Biffin <62933786+Kunggings@users.noreply.github.com>
2025-01-12 17:47:19 +0000

    Hello World Test
    Simple Hello World to test if Arduino is working

---
+++ HelloWorld.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // Initialize the serial communication at a baud rate of 9600
+  Serial.begin(9600);
+
+  // Print "Hello World" to the Serial Monitor
+  Serial.println("Hello World");
+}
+
+void loop() {
+  // Nothing needed here for this example
+}
+
